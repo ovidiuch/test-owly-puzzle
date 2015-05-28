4656f26058a241e27fb4b288761bbe65a98d9396
Ovidiu Cherecheș <skidding@gmail.com>
2015-05-28 19:00:10 +0300

    Update render-blocks.js

--- render-blocks.js
+++ render-blocks.js
@@ -4,3 +4,7 @@ window.renderBlocks = function(blocks) {
     document.getElementById('puzzle').appendChild(blockRenderer());
   }
 };
+
+window.getImagePath = function(index) {
+  return "http://4bf7b4a569b1cd36b775-ebc958ba361dc365f260e879192c467f.r91.cf2.rackcdn.com/" + md5(index) + ".jpg";
+};
