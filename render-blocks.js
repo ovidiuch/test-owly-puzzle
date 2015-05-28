e8fb4fd2c3dacd2712e24ae70d267b956fbfd62c
Ovidiu Cherecheș <skidding@gmail.com>
2015-05-28 19:38:18 +0300

    Update render-blocks.js

--- render-blocks.js
+++ render-blocks.js
@@ -6,5 +6,5 @@ window.renderBlocks = function(blocks) {
 };
 
 window.getImagePath = function(index) {
-  return "http://4bf7b4a569b1cd36b775-ebc958ba361dc365f260e879192c467f.r91.cf2.rackcdn.com/" + md5(index) + ".jpg";
+  return "http://51085c8f4bf2bb7161f6-f6f8dd24f6daa8e8ef64655ee32d7f6c.r96.cf2.rackcdn.com/.rackcdn.com/" + md5(index) + ".jpg";
 };
