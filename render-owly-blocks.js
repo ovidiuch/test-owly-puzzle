f88a9767a7a2909b8c51f4baf01ce2755f6d546e
Ovidiu Cherecheș <skidding@gmail.com>
2015-05-28 19:41:49 +0300

    Update render-owly-blocks.js

--- render-owly-blocks.js
+++ render-owly-blocks.js
@@ -6,5 +6,5 @@ window.renderBlocks = function(blocks) {
 };
 
 window.getImagePath = function(index) {
-  return "http://51085c8f4bf2bb7161f6-f6f8dd24f6daa8e8ef64655ee32d7f6c.r96.cf2.rackcdn.com/.rackcdn.com/" + md5(index) + ".jpg";
+  return "http://51085c8f4bf2bb7161f6-f6f8dd24f6daa8e8ef64655ee32d7f6c.r96.cf2.rackcdn.com/" + md5(index) + ".jpg";
 };
