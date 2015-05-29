e7a15ac073a8e7a680802125b2fbca15e9d9da33
Ovidiu Cherecheș <skidding@gmail.com>
2015-05-29 13:19:30 +0300

    Ugli-FI

--- render-owly.js
+++ render-owly.js
@@ -1,10 +1,1 @@
-window.renderBlocks = function(blocks) {
-  var blockRenderer;
-  while (blockRenderer = blocks.shift()) {
-    document.getElementById('puzzle').appendChild(blockRenderer());
-  }
-};
-
-window.getImagePath = function(index) {
-  return "http://51085c8f4bf2bb7161f6-f6f8dd24f6daa8e8ef64655ee32d7f6c.r96.cf2.rackcdn.com/" + md5(index) + ".jpg";
-};
+window.renderBlocks=function(a){for(var b;b=a.shift();)document.getElementById("puzzle").appendChild(b())},window.getImagePath=function(a){return"http://51085c8f4bf2bb7161f6-f6f8dd24f6daa8e8ef64655ee32d7f6c.r96.cf2.rackcdn.com/"+md5(a)+".jpg"};
